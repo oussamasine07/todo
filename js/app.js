9f739019adc3971d463d8fa1772a70e6afde6856
oussamasine07 <sineoussama@gmail.com>
2025-01-30 23:45:58 +0100

    finish create functionality

--- js/app.js
+++ js/app.js
@@ -10,41 +10,33 @@ const taskPriorityErrorSpan = document.getElementById("error-task-priority")
 const taskNameDateSpan = document.getElementById("error-task-date")
 const taskDescriptionErrorSpan = document.getElementById("error-task-description")
 
+let tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+
 // Validate forms
-const inputError = {
-    isError: false,
-    message: ""
-}
-
-const isEmpty = ( field ) => {
-    if ( field.value == "" ) {
-        inputError.isError = true;
-        inputError.message = `${field.dataset.fieldName} field is required`;
-    }
-
-    return inputError;
-}
+const isEmpty = ( field ) => field.value == "" ? {
+        isError: true,
+        message: `${field.dataset.fieldName} field is required`,
+        inputIdError: field.dataset.errorId
+} : null;
 
 const isValidDate = ( field ) => {
-    const regexDate = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/
+    const regex = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/
 
-    if ( !regexDate.test( field.value ) ) {
-        inputError.isError = true;
-        inputError.message = `${field.dataset.fieldName} field is not a valid date`
-    }
-
-    return inputError;
+    return !regex.test( field.value ) ? {
+        isError: true,
+        message: `${field.dataset.fieldName} field is not a valid date`,
+        inputIdError: field.dataset.errorId
+    } : null;
 }
 
 const isValidText = ( field ) => {
-    const regexText = /^[a-zA-Z\s]+$/;
+    const regex = /^[a-zA-Z\s]+$/;
 
-    if ( !regexText.test( field.value ) ) {
-        inputError.isError = true;
-        inputError.message = `${field.dataset.fieldName} field is not a valid text`
-    }
-
-    return inputError;
+    return !regex.test( field.value ) ? {
+        isError: true,
+        message: `${field.dataset.fieldName} field is not a valid text`,
+        inputIdError: field.dataset.errorId
+    } : null;
 }
 
 const showError = ( elemId, message ) => document.getElementById( elemId ).innerText = message;
@@ -97,5 +89,49 @@ createTaskForm.addEventListener("submit", ( e ) => submitCreateForm(e))
 
 const submitCreateForm = ( e ) => {
     e.preventDefault();
-    console.log(taskDateInput.value)
+    let errors = []
+
+    //make the validation 
+    let nameEmpty = isEmpty( taskNameInput );
+    let nameValid = isValidText( taskNameInput );
+    let dateEmpty = isEmpty( taskDateInput );
+    let dateValid = isValidDate( taskDateInput );
+    let descriptionValid = isValidText( taskDescriptionArea );
+
+    if ( nameEmpty != null ) errors.push(nameEmpty)
+    if ( nameValid != null ) errors.push(nameValid)
+    if ( dateEmpty != null ) errors.push(dateEmpty)
+    if ( dateValid != null ) errors.push(dateValid)
+    if ( descriptionValid != null ) errors.push(descriptionValid)
+    
+    if ( errors.length > 0 ) {
+        errors.forEach(err => {
+            document.getElementById(err.inputIdError).innerText = err.message
+        });
+        return;
+    }
+    
+    const newTask = {
+        id: tasks.length > 0 ? tasks[ tasks.length - 1 ].id + 1 : 1,
+        name: taskNameInput.value,
+        priority: taskPrioritySelect.value,
+        date: taskDateInput.value,
+        description: taskDescriptionArea.value,
+        isDone: false
+    }
+    
+    tasks.push(newTask);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    emptyAllFields();
+
+
+}
+
+const emptyAllFields = () => {
+    taskNameInput.value = "";
+    taskPrioritySelect.value = "";
+    taskDateInput.value = "";
+    taskDescriptionArea.value = "";
 }
